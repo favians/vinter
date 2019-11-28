04faf1ae96a1f3f667d82dc70eb866cabf24778e
faviansyah <faviansyah@aterra.id>
2019-11-28 10:12:01 +0700

    add special Case

--- blueprints/Intern/model.py
+++ blueprints/Intern/model.py
@@ -16,6 +16,30 @@ class Intern(db.Model):
     pendidikan = db.Column(db.String(1024), nullable=True)
     deskripsi = db.Column(db.String(2048), nullable=True)
 
+    kecamatan = db.Column(db.String(255), nullable=True)
+    kabupaten = db.Column(db.String(255), nullable=True)
+    desa_kelurahan = db.Column(db.String(255), nullable=True)
+    rt = db.Column(db.Integer, nullable=True)
+    rw = db.Column(db.Integer, nullable=True)
+    nik = db.Column(db.Integer, nullable=True)
+    jenis_kelamin = db.Column(db.String(255), nullable=True)
+    tempat_lahir = db.Column(db.String(255), nullable=True)
+    tanggal_lahir = db.Column(db.String(255), nullable=True)
+    jurusan = db.Column(db.String(255), nullable=True)
+    status_perkawinan = db.Column(db.String(255), nullable=True)
+    nama_ayah = db.Column(db.String(255), nullable=True)
+    nama_ibu = db.Column(db.String(255), nullable=True)
+    nama_perusahaan = db.Column(db.String(255), nullable=True)
+    jabatan = db.Column(db.String(255), nullable=True)
+    mulai_tahun = db.Column(db.String(255), nullable=True)
+    lama_kerja = db.Column(db.String(255), nullable=True)
+    provinsi = db.Column(db.String(255), nullable=True)
+    kota_kabupaten = db.Column(db.String(255), nullable=True)
+    kecamatan_penempatan = db.Column(db.String(255), nullable=True)
+    desa_kelurahan_penempatan = db.Column(db.String(255), nullable=True)
+
+
+
     response_field = {
         'id': fields.Integer,
         'created_at': fields.DateTime,
@@ -27,9 +51,31 @@ class Intern(db.Model):
         'email': fields.String,
         'pendidikan': fields.String,
         'deskripsi': fields.String,
+
+        'kecamatan' : fields.String,
+        'kabupaten' : fields.String,
+        'desa_kelurahan' : fields.String,
+        'rt' : fields.Integer,
+        'rw' : fields.Integer,
+        'nik' : fields.Integer,
+        'jenis_kelamin' : fields.String,
+        'tempat_lahir' : fields.String,
+        'tanggal_lahir' : fields.String,
+        'jurusan' : fields.String,
+        'status_perkawinan' : fields.String,
+        'nama_ayah' : fields.String,
+        'nama_ibu' : fields.String,
+        'nama_perusahaan' : fields.String,
+        'jabatan' : fields.String,
+        'mulai_tahun' : fields.String,
+        'lama_kerja' : fields.String,
+        'provinsi' : fields.String,
+        'kota_kabupaten' : fields.String,
+        'kecamatan_penempatan' : fields.String,
+        'desa_kelurahan_penempatan' : fields.String
     }
 
-    def __init__(self, created_at, name, password, image, address, active, role, email, pendidikan, deskripsi):
+    def __init__(self, created_at, name, password, image, address, active, role, email, pendidikan, deskripsi, kecamatan, kabupaten, desa_kelurahan, rt, rw, nik, jenis_kelamin, tempat_lahir, tanggal_lahir, jurusan, status_perkawinan, nama_ayah, nama_ibu, nama_perusahaan, jabatan, mulai_tahun, lama_kerja, provinsi, kota_kabupaten, kecamatan_penempatan, desa_kelurahan_penempatan):
         self.created_at = created_at
         self.name = name
         self.password = password
@@ -40,6 +86,29 @@ class Intern(db.Model):
         self.email = email
         self.pendidikan = pendidikan
         self.deskripsi = deskripsi
+
+        self.kecamatan = kecamatan
+        self.kabupaten = kabupaten
+        self.desa_kelurahan = desa_kelurahan
+        self.rt = rt
+        self.rw = rw
+        self.nik = nik
+        self.jenis_kelamin = jenis_kelamin
+        self.tempat_lahir = tempat_lahir
+        self.tanggal_lahir = tanggal_lahir
+        self.jurusan = jurusan
+        self.status_perkawinan = status_perkawinan
+        self.nama_ayah = nama_ayah
+        self.nama_ibu = nama_ibu
+        self.nama_perusahaan = nama_perusahaan
+        self.jabatan = jabatan
+        self.mulai_tahun = mulai_tahun
+        self.lama_kerja = lama_kerja
+        self.provinsi = provinsi
+        self.kota_kabupaten = kota_kabupaten
+        self.kecamatan_penempatan = kecamatan_penempatan
+        self.desa_kelurahan_penempatan = desa_kelurahan_penempatan
+
 
     def __repr__(self):
         return '<Intern %r>' % self.id
--- blueprints/Intern/resources.py
+++ blueprints/Intern/resources.py
@@ -11,6 +11,13 @@ from flask_jwt_extended import jwt_required, get_jwt_claims
 
 #password Encription
 from password_strength import PasswordPolicy
+#Blueprint Collection
+from blueprints.Company.model import Company
+from blueprints.Certificate.model import Certificate
+from blueprints.Position.model import Position
+from blueprints.Intern.model import Intern
+from blueprints.Task.model import Task
+from blueprints.OngoingPosition.model import OngoingPosition
 
 # 'intern' penamaan (boleh diganti)
 bp_intern = Blueprint('intern', __name__)
@@ -46,6 +53,28 @@ class InternResource(Resource):
         parser.add_argument('address', location='json')
         parser.add_argument('pendidikan', location='json')
         parser.add_argument('deskripsi', location='json')
+
+        parser.add_argument('kecamatan', location='json')
+        parser.add_argument('kabupaten', location='json')
+        parser.add_argument('desa_kelurahan', location='json')
+        parser.add_argument('rt', location='json')
+        parser.add_argument('rw', location='json')
+        parser.add_argument('nik', location='json')
+        parser.add_argument('jenis_kelamin', location='json')
+        parser.add_argument('tempat_lahir', location='json')
+        parser.add_argument('tanggal_lahir', location='json')
+        parser.add_argument('jurusan', location='json')
+        parser.add_argument('status_perkawinan', location='json')
+        parser.add_argument('nama_ayah', location='json')
+        parser.add_argument('nama_ibu', location='json')
+        parser.add_argument('nama_perusahaan', location='json')
+        parser.add_argument('jabatan', location='json')
+        parser.add_argument('mulai_tahun', location='json')
+        parser.add_argument('lama_kerja', location='json')
+        parser.add_argument('provinsi', location='json')
+        parser.add_argument('kota_kabupaten', location='json')
+        parser.add_argument('kecamatan_penempatan', location='json')
+        parser.add_argument('desa_kelurahan_penempatan', location='json')
         args = parser.parse_args()
 
         validation = policy.test(args['password'])
@@ -53,7 +82,7 @@ class InternResource(Resource):
         if validation == []:
             password_digest = hashlib.md5(args['password'].encode()).hexdigest()
 
-            intern = Intern(datetime.datetime.now(), args['name'], password_digest, args['image'], args['address'], True, 'intern', args['email'], args['pendidikan'], args['deskripsi'])
+            intern = Intern(datetime.datetime.now(), args['name'], password_digest, args['image'], args['address'], True, 'intern', args['email'], args['pendidikan'], args['deskripsi'], args['kecamatan'], args['kabupaten'], args['desa_kelurahan'], args['rt'], args['rw'], args['nik'], args['jenis_kelamin'], args['tempat_lahir'], args['tanggal_lahir'], args['jurusan'], args['status_perkawinan'], args['nama_ayah'], args['nama_ibu'], args['nama_perusahaan'], args['jabatan'], args['mulai_tahun'], args['lama_kerja'], args['provinsi'], args['kota_kabupaten'], args['kecamatan_penempatan'], args['desa_kelurahan_penempatan'])
             
             try:
                 db.session.add(intern)
@@ -126,7 +155,59 @@ class InternList(Resource):
         
         return {"status":"success", "result":result}, 200, {'Content-Type':'application/json'}
 
+class InternSpecialCase(Resource):
+
+    def options(self):
+        return {},200
+
+    def __init__(self):
+        pass
+
+    @jwt_required
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('p', type=int, location='args', default=1)
+        parser.add_argument('rp',type=int, location='args', default=25)
+        parser.add_argument("kecamatan", location = "args")
+        parser.add_argument("kabupaten", location = "args")
+        parser.add_argument("kota_kabupaten", location = "args")
+        parser.add_argument("pendidikan", location = "args")
+        parser.add_argument("jurusan", location = "args")
+        args =parser.parse_args()
+
+        offset = (args['p'] * args['rp']) - args['rp']
+
+        qry = Intern.query
+
+        if args['kecamatan'] is not None:
+            qry = qry.filter(Intern.kecamatan.like("%"+args["kecamatan"]+"%"))
+        if args['kabupaten'] is not None:
+            qry = qry.filter(Intern.kabupaten.like("%"+args["kabupaten"]+"%"))
+        if args['kota_kabupaten'] is not None:
+            qry = qry.filter(Intern.kota_kabupaten.like("%"+args["kota_kabupaten"]+"%"))
+        if args['pendidikan'] is not None:
+            qry = qry.filter(Intern.pendidikan.like("%"+args["pendidikan"]+"%"))
+        if args['jurusan'] is not None:
+            qry = qry.filter(Intern.jurusan.like("%"+args["jurusan"]+"%"))
+
+        result = []
+        for row in qry.limit(args['rp']).offset(offset).all():
+            internValue = marshal(row,Intern.response_field)
+
+            OnPosQry = OngoingPosition.query.filter_by(intern_id=internValue["id"]).all()
+            CertificateQry = Certificate.query.filter_by(intern_id=internValue["id"]).all()
+
+            OnPosQryValue = marshal(OnPosQry, OngoingPosition.response_field)
+            CertQryValue = marshal(CertificateQry,Certificate.response_field)
+
+            internValue["position_history"] = OnPosQryValue
+            internValue["certificate"] = CertQryValue
+
+            result.append(internValue)
+        
+        return {"status":"success", "result":result}, 200, {'Content-Type':'application/json'}
 
 
 api.add_resource(InternResource, '', '')
 api.add_resource(InternList,'','/list')
+api.add_resource(InternSpecialCase,'','/specialmetadata')
